9cc8e92675d81f1e9f2f46f765384dc92d9e3ac5
Barry Stace <barrystace@yahoo.co.nz>
2019-09-18 14:35:31 +1200

    New Page

---
+++ indexRedone.php
@@ -0,0 +1,22 @@
+<!-- This is your main page -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Tyre Town</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    Test
+</body>
+
+</html>
