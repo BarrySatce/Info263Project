56fdb6824b12627d2bc887cee1c91d874f5e481f
Barry Stace <barrystace@yahoo.co.nz>
2019-09-20 12:29:33 +1200

    Added page formatting, butttons

--- indexRedone.php
+++ indexRedone.php
@@ -9,10 +9,9 @@
     <title>Tyre Town</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
 
     <!-- css style sheet -->
     <link rel="stylesheet" href="css/styles.css">
@@ -22,17 +21,72 @@
 
 </head>
 <body>
-    <div class="header">
-        <h1>Tyre Town</h1>
+<!-- This container contains everything on the page
+     The page is split up into 4 'Rows', each with their own amount of columns-->
+<div class="container">
+
+    <!-- This is the first row, it will contain the logo, and the page Header (or a banner if we choose) -->
+    <div class="row my-5" id="header">
+        <div class="col-2">Image goes here</div>
+        <div class="col-10">Banner/Header Goes here</div>
     </div>
-    <div class="header">
-        <h2>Tyre Town Invoice Search</h2>
+
+    <!-- This is the second row, it will contain the navigation bar -->
+    <div class="row my-5 my-auto text-center" id="navigation">
+        <div class="col-4 border">Home Button</div>
+        <div class="col-4 border">Spare Button</div>
+        <div class="col-4 border">Spare Button</div>
     </div>
 
+    <!-- This is the third row, it will have the search bar -->
+    <div class="row my-5" id="search">
+        <div class="col my-auto text-center">Search Bar</div>
+    </div>
 
-
+    <!-- This is the forth row, it will have all the invoice options on the left,
+         when on is clicked a table will be shown to the right containing that info -->
+    <div class="row my-5" id="main">
+        <div class="col-3">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
+                   aria-controls="v-pills-home" aria-selected="true">Customer</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
+                   aria-controls="v-pills-profile" aria-selected="false">Invoice</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
+                   aria-controls="v-pills-messages" aria-selected="false">Garage Info</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
+                   aria-controls="v-pills-settings" aria-selected="false">Allignment Info</a>
+            </div>
+        </div>
+        <div clas="col-9">
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
+                     aria-labelledby="v-pills-home-tab">aaaaaaaaaa..
+                </div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    ...
+                </div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                    ...
+                </div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 
 
 </body>
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </html>
